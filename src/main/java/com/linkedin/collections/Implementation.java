99e3b61dc3fda016d7dd36af726ddc166f44f17b
anirudh-1729 <anirudhmohanty.15april@gmail.com>
2024-07-04 23:10:06 +0530

    .

--- src/main/java/com/linkedin/collections/Implementation.java
+++ src/main/java/com/linkedin/collections/Implementation.java
@@ -1,6 +1,25 @@
 package com.linkedin.collections;
 
-public class Implementation implements Contract  {
+public class Implementation implements Contract   {
+
+    @Override
+    public void term1() {
+        // TODO Auto-generated method stub
+        System.out.println("Term 1 Contract interface");
+    }
+
+    @Override
+    public void term2() {
+        // TODO Auto-generated method stub
+        System.out.println("Term 2 Contract interface");
+    }
+
+    @Override
+    public void extendedTerm() {
+        // TODO Auto-generated method stub
+        System.out.println("Extended Contract interface");
+    }
+
 
 
 }
